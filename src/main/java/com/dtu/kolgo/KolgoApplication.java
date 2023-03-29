41b844e04247841e98240b9cde150adec1383831
Sang Do <svngdo@gmail.com>
2023-03-29 18:38:00 +0700

    Initial commit

---
+++ src/main/java/com/dtu/kolgo/KolgoApplication.java
@@ -0,0 +1,13 @@
+package com.dtu.kolgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KolgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KolgoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dtu/kolgo/KolgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dtu.kolgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KolgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
