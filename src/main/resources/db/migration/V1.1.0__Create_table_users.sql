b73f6b79722c86c482381129ab03787b7f9f1961
Sang Do <svngdo@gmail.com>
2023-04-08 20:09:52 +0700

    Remove users username from migration

--- src/main/resources/db/migration/V1.1.0__Create_table_users.sql
+++ src/main/resources/db/migration/V1.1.0__Create_table_users.sql
@@ -1,10 +1,9 @@
 create table if not exists users
 (
     id         int generated always as identity primary key,
-    username   varchar(50)  not null unique,
+    email      varchar(50)  not null unique,
+    password   varchar(255) not null,
     first_name varchar(20)  not null,
     last_name  varchar(20)  not null,
-    avatar     varchar      null,
-    email      varchar(50)  not null unique,
-    password   varchar(255) not null
+    avatar     varchar      null
 );
