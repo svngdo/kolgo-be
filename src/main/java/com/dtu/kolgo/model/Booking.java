6091dd85d4b1038f24c2f23ec9443c880048b770
Sang Do <svngdo@gmail.com>
2023-04-28 20:11:25 +0700

    add Booking CRUD

--- src/main/java/com/dtu/kolgo/model/Booking.java
+++ src/main/java/com/dtu/kolgo/model/Booking.java
@@ -3,7 +3,7 @@ package com.dtu.kolgo.model;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.List;
 
 @NoArgsConstructor
@@ -16,15 +16,16 @@ import java.util.List;
 public class Booking extends BaseInt {
 
     @Column
-    private LocalDateTime time;
-    @Column
-    private int cost;
+    private LocalDate date;
     @ManyToOne
     @JoinColumn(name = "enterprise_id")
     private Enterprise enterprise;
     @ManyToOne
     @JoinColumn(name = "kol_id")
     private Kol kol;
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "payment_id")
+    private Payment payment;
     @OneToMany(mappedBy = "booking", cascade = CascadeType.ALL)
     private List<Feedback> feedbacks;
 
