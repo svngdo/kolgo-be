286cbc726a837b6a275ce716e227cdd36f303e39
Sang Do <svngdo@gmail.com>
2023-05-18 14:53:58 +0700

    fix resource handler, add resource and icon path

--- src/main/java/com/dtu/kolgo/config/WebConfig.java
+++ src/main/java/com/dtu/kolgo/config/WebConfig.java
@@ -8,20 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Value("${file.image-path}")
-    private String imagePath;
-
-    @Value("${file.icon-path}")
-    private String iconPath;
+    @Value("${file.resource-path}")
+    private String resourcePath;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-                .addResourceHandler("/images/**")
-                .addResourceLocations("file:///" + imagePath);
-
-        registry.addResourceHandler("/icons/**")
-                .addResourceLocations("file:///" + iconPath);
+        registry.addResourceHandler("/**")
+                .addResourceLocations("file:///" + resourcePath);
     }
 
 }
